f0721e9ee50776f6fe9d92d5cff237289eeb9990
temusqrlfrvr-sys <temusqrlfrvr@gmail.com>
2026-09-04 16:58:15 -0400

    v1.2

---
+++ sw.js
@@ -0,0 +1,24 @@
+// sw.js - Service Worker for OS-Level Notifications
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+            for (const client of clientList) {
+                if ('focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow('./');
+            }
+        })
+    );
+});
